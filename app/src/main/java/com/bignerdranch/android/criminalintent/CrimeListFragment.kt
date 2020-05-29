f408b5c13d2fdeb52b01b7626086116de3e4723a
Lane Young <youngla13@students.ecu.edu>
2020-05-29 12:09:34 -0400

    Chapter 10 complete challenge data

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent
 
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -76,7 +77,8 @@ class CrimeListFragment : Fragment() {
         fun bind(crime: Crime) {
             this.crime = crime
             titleTextView.text = this.crime.title
-            dateTextView.text = this.crime.date.toString()
+            val dateString = DateFormat.getMediumDateFormat(context).format(this.crime.date)
+            dateTextView.text = dateString
             solvedImageView.visibility = if(crime.isSolved) {
                 View.VISIBLE
             } else {
